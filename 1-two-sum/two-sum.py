141bac3c0e0606061f73111f91c2d5696074f78c
Sarvesh <66882974+sarvesh-tech@users.noreply.github.com>
2024-06-03 13:02:58 -0500

    Time: 50 ms (94.38%) | Memory: 17.5 MB (60.18%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
-        #create a hasmap key:value is num:index
-
-        #return index pair that add up to the target value
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-
         for i in range(len(nums)):
             complement = target - nums[i]
-
             if complement in seen:
-                return [i, seen[complement]]
-            
-            seen[nums[i]] = i 
+                return [seen[complement], i]
+            seen[nums[i]] = i
 
-            
+
